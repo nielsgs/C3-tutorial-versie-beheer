f3f1127c28c0a33ca08c87bf26f0af4d7feba7e7
GuusJansma <19088051@student.hhs.nl>
2021-05-10 19:54:01 +0200

    Begin van de code gemaakt

---
+++ ProjectCode/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Dit is het begin van de code waar iedereen iets aan zal schrijven:\n");
+    //Guus
+    int x = 13;
+    int y = 277;
+
+    int g = x + y;
+    printf("%d", g);
+    //Freek
+
+    //Jasper
+
+    //Tijn
+
+    //Cas
+
+    //Niels
+
+
+    return 0;
+}
